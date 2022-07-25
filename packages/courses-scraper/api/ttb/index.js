b8c221b76daf10cdf395dd919c98024fb577ae4f
TheFatPanda97 <shawnhu97@gmail.com>
2022-07-25 17:39:39 -0400

    added code to scrape all course codes

--- packages/courses-scraper/api/ttb/index.js
+++ packages/courses-scraper/api/ttb/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import fs from 'fs';
+import ora from 'ora';
 
 const url = 'https://api.easi.utoronto.ca/ttb/getPageableCourses';
 const body = {
@@ -19,11 +21,41 @@ const body = {
   divisions: ['APSC', 'ARCLA', 'ARTSC', 'ERIN', 'MUSIC', 'SCAR'],
   creditWeights: [],
   page: 1,
-  pageSize: 10,
+  pageSize: 9999, // hack to get all courses
   direction: 'asc',
 };
 
 (async () => {
-  const result = await axios.post(url, body);
-  console.log(result.data);
+  const spinner = ora({
+    text: 'Retrieving Courses',
+    spinner: {
+      interval: 80,
+      frames: ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'],
+    },
+  }).start();
+
+  try {
+    const allCourseCodes = [];
+    const result = await axios.post(url, body);
+    const courses = result.data.payload.pageableCourse.courses;
+    console.log(courses);
+
+    courses.forEach(({ code, sectionCode, name }) => {
+      allCourseCodes.push({
+        courseCode: code + sectionCode,
+        name,
+      });
+    });
+
+    fs.writeFile(`output/allCourseCodes.json`, JSON.stringify(allCourseCodes), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    spinner.stop();
+  } catch (error) {
+    spinner.stop();
+    console.error(error);
+  }
 })();
